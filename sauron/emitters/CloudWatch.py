9cf86b49e23bd6a1d690721bebcdf85d125a9eb4
Dan Lecocq <dan@seomoz.org>
2011-10-13 14:18:15 -0700

    Logger bug.

--- sauron/emitters/CloudWatch.py
+++ sauron/emitters/CloudWatch.py
@@ -98,13 +98,13 @@ class CloudWatch(Emitter.Emitter):
 		try:
 			self.dims['InstanceId'] = urllib2.urlopen('http://169.254.169.254/1.0/meta-data/instance-id', timeout=1.0).read().strip()
 		except urllib2.URLError:
-			self.logger.warn('Failed to get an instance ID for this node from Amazon')
+			logger.warn('Failed to get an instance ID for this node from Amazon')
 	
 	def metrics(self, metrics):
 		t = datetime.datetime.now()
 		for name, results in metrics.items():
 			for key,value in results['results'].items():
-				self.logger.info('Pushing %s-%s => %s' % (name, key, repr(value)))
+				logger.info('Pushing %s-%s => %s' % (name, key, repr(value)))
 				v, u = value
 				self.conn.put_metric_data(self.namespace, name + '-' + key, timestamp=t, unit=u, value=v, dimensions=self.dims)
 	
