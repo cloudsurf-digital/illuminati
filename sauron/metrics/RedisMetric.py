e7453fd4f0b21f1bc77a4794e18d2ac070aa9eae
Dan Lecocq <dan@seomoz.org>
2011-10-19 10:52:37 -0700

    Added hlen, scard, hget to redis.

--- sauron/metrics/RedisMetric.py
+++ sauron/metrics/RedisMetric.py
@@ -33,11 +33,11 @@ class RedisMetric(Metric.Metric):
 		'client_longest_output_list': lambda x: (x, 'Count'),
 		'client_biggest_input_buf'  : lambda x: (x, 'Bytes'),
 		'blocked_clients'           : lambda x: (x, 'Count'),
-		'used_memory'               : lambda x: parseMemroy(x),
-		'used_memory_human'         : lambda x: parseMemroy(x),
-		'used_memory_rss'           : lambda x: parseMemroy(x),
-		'used_memroy_peak'          : lambda x: parseMemroy(x),
-		'used_memory_peak_human'    : lambda x: parseMemroy(x),
+		'used_memory'               : lambda x: parseMemory(x),
+		'used_memory_human'         : lambda x: parseMemory(x),
+		'used_memory_rss'           : lambda x: parseMemory(x),
+		'used_memroy_peak'          : lambda x: parseMemory(x),
+		'used_memory_peak_human'    : lambda x: parseMemory(x),
 		'mem_fragmentation_ratio'   : lambda x: (x, 'None'),
 		'loading'                   : lambda x: (x, 'None'),
 		'aof_enabled'               : lambda x: (x, 'None'),
@@ -76,9 +76,14 @@ class RedisMetric(Metric.Metric):
 		self.info   = kwargs.get('info'  , [])
 		# The keys we should get and interpret as numbers
 		self.get    = kwargs.get('get'   , [])
-		print '%s' % repr(self.get)
 		# The keys we should get, and report their length
-		self.length = kwargs.get('length', [])
+		self.llen   = kwargs.get('llen', [])
+		# The keys we should get and report the hash length
+		self.hlen   = kwargs.get('hlen', [])
+		# The keys we should get and report the particular key from
+		self.hget   = kwargs.get('hget', {})
+		# The keys we should get and report the cardinality of
+		self.scard  = kwargs.get('scard', [])
 	
 	def values(self):
 		try:
@@ -91,12 +96,21 @@ class RedisMetric(Metric.Metric):
 					results[i] = (info[i], 'None')
 			
 			both = list(self.get)
-			both.extend(self.length)
+			both.extend(self.llen)
+			both.extend(self.hlen)
+			both.extend(['%s-%s' % (k, v) for k,v in self.hget.items()])
+			both.extend(self.scard)
 			with self.redis.pipeline() as pipe:
 				for g in self.get:
 					pipe.get(g)
-				for l in self.length:
+				for l in self.llen:
 					pipe.llen(l)
+				for h in self.hlen:
+					pipe.hlen(h)
+				for k,v in self.hget.items():
+					pipe.hget(k, v)
+				for s in self.scard:
+					pipe.scard(s)
 				fetched = pipe.execute()
 				for index in range(len(fetched)):
 					results[both[index]] = (fetched[index], 'Count')
