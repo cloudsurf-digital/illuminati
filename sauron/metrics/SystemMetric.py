6ee0f6b209c02ccb448ec984a487ee058f7ffb99
Takahiro Fujiwara <wutali.no@gmail.com>
2012-10-26 16:05:32 +0900

    Added load average metrics on SystemMetric class.

--- sauron/metrics/SystemMetric.py
+++ sauron/metrics/SystemMetric.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
-# 
+#
 # Copyright (c) 2011 SEOmoz
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,6 +21,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import os
 import psutil
 import datetime
 from sauron import logger
@@ -29,11 +30,19 @@ from sauron.metrics import Metric, MetricException
 class SystemMetric(Metric):
     def values(self):
         try:
+            loadavg1, loadavg5, loadavg15 = os.getloadavg()
+        except OSError:
+            loadavg1, loadavg5, loadavg15 = 0, 0, 0
+
+        try:
             phys = psutil.phymem_usage()
             virt = psutil.virtmem_usage()
             return {'results': {
                     'physical': (phys.percent, 'Percent'),
-                    'virtual' : (virt.percent, 'Percent')
+                    'virtual' : (virt.percent, 'Percent'),
+                    'loadavg1': (loadavg1, 'Processes'),
+                    'loadavg5': (loadavg5, 'Processes'),
+                    'loadavg15': (loadavg15, 'Processes'),
                 }
             }
         except OSError as e:
