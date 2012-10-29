dfa1c35e8f0522fe588cb2f4ae79deec8729a407
Takahiro Fujiwara <wutali.no@gmail.com>
2012-10-29 13:35:07 +0900

    Renamed key name of freeInodes with free_inodes.

--- sauron/metrics/DiskMetric.py
+++ sauron/metrics/DiskMetric.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
-# 
+#
 # Copyright (c) 2011 SEOmoz
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,11 +30,11 @@ class DiskMetric(Metric):
     def __init__(self, name, path, **kwargs):
         Metric.__init__(self, name, **kwargs)
         self.reconfig(name, path, **kwargs)
-    
+
     def reconfig(self, name, path, **kwargs):
         Metric.reconfig(self, name, **kwargs)
         self.path = path
-    
+
     def values(self):
         # Reference:
         # http://stackoverflow.com/questions/787776/find-free-disk-space-in-python-on-os-x
@@ -50,7 +50,7 @@ class DiskMetric(Metric):
                     'used'      : (round(used , 3), 'Gigabytes'),
                     'percent'   : (round(float(used) / float(total), 3), 'Percent'),
                     'inodes'    : (st.f_files, 'Count'),
-                    'freeInodes': (st.f_ffree, 'Count')
+                    'free_inodes': (st.f_ffree, 'Count')
             }
             return {'results': results}
         except Exception as e:
