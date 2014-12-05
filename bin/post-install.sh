a60ef9a033fd054083332f09163a6ab501c654bf
Lasse Borchard <lasse.borchard@prosiebensat1digital.de>
2014-12-05 13:53:01 +0100

    sauron writes its pidfile, not the init-scipt

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/sbin/chkconfig --add sauron > /dev/null
-/sbin/chkconfig --levels 345 sauron on > /dev/null
+/sbin/chkconfig --add sauron > /dev/null 2>&1
+/sbin/chkconfig --levels 345 sauron on > /dev/null 2>&1
--- bin/pre-uninstall.sh
+++ bin/pre-uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ -f /etc/init.d/sauron ] && /etc/init.d/sauron stop || true
-/sbin/chkconfig --list sauron | grep -q sauron && /sbin/chkconfig --del sauron || true
+[ -f /etc/init.d/sauron ] && /etc/init.d/sauron stop > /dev/null 2>&1 || true
+/sbin/chkconfig --list sauron | grep -q sauron && /sbin/chkconfig --del sauron > /dev/null 2>&1 || true
